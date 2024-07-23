e468acfa01cbb06961ffdd453cb2758725788f9e
Munafkhanusiya <munafkhanusiya26@gmail.com>
2024-07-23 14:30:11 +0530

    first

---
+++ bootstrape/task-1/assets/js/main.js
@@ -0,0 +1,15 @@
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }, 
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
